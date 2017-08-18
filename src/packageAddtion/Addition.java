3755a30d5dec5235e8e9f96af0bf025a31c2bf80
user1 <user1@user>
2017-08-18 14:12:34 +0100

    Addition of two number without + operator.

---
+++ src/packageAddtion/Addition.java
@@ -0,0 +1,14 @@
+package packageAddtion;
+
+public class Addition {
+
+	 public static void main(String[] args) {
+		
+		 int a = 10, b = 10;// two variables with initialized value of 10 each
+		 
+		 int c = a- (-b);// because - - is equal to plus
+		 
+		 System.out.println(" a plus b is " + c);// lets print value
+	}
+
+}
